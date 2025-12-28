0840326e0063a6d348004637f12e5ca9c8007118
Khushi Chetule <khushichetule2@gmail.com>
2025-12-28 18:43:05 +0530

    Initialized the pricing page

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { appPlans } from '../assets/assets';
+
+interface Plan {
+  id: string;
+  name: string;
+  price: string;
+  credites: number;
+  description: string;
+  features: string[];
+}
 
 const Pricing = () => {
+  const [plans] = React.useState<Plan[]>(appPlans)
   return (
-    <div>
-        <h1>Pricing</h1>
-    </div>
+    <>
+       <div className='w-full max-w-5xl mx-auto z-20 max-md:px-4 min-h[80vh]'>
+        <div className='text-center mt-16'>
+          <h1 className='text-gray-100 text-3xl font-medium'>Choose Your Plan</h1>
+          <p className='text-gray-400 text-sm max-w-md mx-auto mt-2'>Start for free and scale up as you grow, Find the perfect plan for your content creation needs.</p>
+        </div>
+
+       </div>
+    </>
   )
 }
 
